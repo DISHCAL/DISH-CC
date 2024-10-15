261b18311dea6f4b2475fad71b7d61f1798a0204
DISHCAL <fitim.vojvoda@metro.de>
2024-10-15 10:01:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ function calculateCosts() {
         simServiceFee = 3.90;
     }
 
-    // Monatliche Gesamtkosten DISH PAY
+    // Berechnung der monatlichen Gesamtkosten (Disagio + Transaktionsgebühren + Hardwarekosten + SIM/Servicegebühr)
     const totalMonthlyCost = totalDisagioFees + transactionFee + hardwareMonthlyCost + simServiceFee;
 
     // Einmalige Gesamtkosten DISH PAY
@@ -131,21 +131,8 @@ function calculateCosts() {
         document.getElementById('oneTimeCost').innerText = "";  // Keine einmaligen Kaufkosten bei Miete
     }
 
-    // Disagio-Gebühren
-    document.getElementById('totalDisagioFees').innerText = `Disagio-Gebühren pro Monat: ${totalDisagioFees.toFixed(2)} €`;
-
-    // Transaktionsgebühren
-    document.getElementById('transactionFee').innerText = `Transaktionsgebühren (0,06 € pro Transaktion): ${transactionFee.toFixed(2)} €`;
-
-    // SIM/Servicegebühr
-    if (simServiceFee > 0) {
-        document.getElementById('simServiceFee').innerText = `SIM/Servicegebühr: ${simServiceFee.toFixed(2)} €`;
-    } else {
-        document.getElementById('simServiceFee').innerText = ""; // Leer lassen, wenn keine Gebühr anfällt
-    }
-
-    // Monatliche Gesamtkosten
-    document.getElementById('totalCost').innerText = `Monatliche Gesamtkosten DISH PAY: ${totalMonthlyCost.toFixed(2)} €`;
+    // Nur eine Zeile für die monatlichen Gesamtkosten anzeigen
+    document.getElementById('totalCost').innerText = `Monatliche Gebühren: ${totalMonthlyCost.toFixed(2)} €`;
 
     // Wettbewerberberechnung (nur bei ausführlicher Berechnung)
     if (calculationType === 'ausführlich') {
@@ -205,8 +192,6 @@ Dieses Angebot ist unverbindlich und dient ausschließlich zu Informationszwecke
 Ergebnisse:
 ${document.getElementById('oneTimeCost').innerText}
 ${document.getElementById('monthlyCost').innerText}
-${document.getElementById('totalDisagioFees').innerText}
-${document.getElementById('transactionFee').innerText}
 ${simServiceFeeText}
 ${document.getElementById('totalCost').innerText}
 ${document.getElementById('competitorTotal').innerText}
